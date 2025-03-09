1b4088f0ff286f10fde404f30b81177a0ce5d61c
sudheerk <saysudheer@gmail.com>
2025-03-10 01:01:53 +0530

    region change

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 # Define variables
 FUNCTION_ARN="arn:aws:lambda:us-west-2:466733620502:function:cicdLambdatest"
 ZIP_FILE="lambda_function.zip"
-AWS_REGION="us-east-1"
+AWS_REGION="us-west-2"
 
 # Install dependencies
 pip install -r requirements.txt -t package
