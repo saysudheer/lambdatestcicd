955b7ab3d8c0be3483687859eefc4295e6e34112
sudheerk <saysudheer@gmail.com>
2025-03-10 00:36:48 +0530

    test lambda ci cd

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+# Define variables
+FUNCTION_NAME="JokesLambdaFunctionTest"
+ZIP_FILE="lambda_function.zip"
+AWS_REGION="us-east-1"
+
+# Install dependencies
+pip install -r requirements.txt -t package
+
+# Create deployment package
+cd package
+zip -r ../$ZIP_FILE .
+cd ..
+zip -g $ZIP_FILE lambda_function.py
+
+# Deploy the function
+aws lambda update-function-code --function-name $FUNCTION_NAME --zip-file fileb://$ZIP_FILE --region $AWS_REGION
---
+++ lambda_function.py
@@ -0,0 +1,22 @@
+import json
+
+def lambda_handler(event, context):
+    print(f"received event::{event}")
+    joke_api_url = "https://official-joke-api.appspot.com/random_joke"
+    
+    try:
+        response = requests.get(joke_api_url)
+        joke_data = response.json()
+        
+        joke = f"{joke_data['setup']} - {joke_data['punchline']}"
+        
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"joke": joke})
+        }
+    
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
